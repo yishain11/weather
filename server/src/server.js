a27f34b9aa1eb3066c057b0b0f3e156a87c71e3b
yishai-n-11 <yishain11@gmail.com>
2023-02-23 13:54:37 +0200

    added data

--- server/src/server.js
+++ server/src/server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
-
 const path = require('path');
-const port = 5454;
+
+const port = process.env.PORT;
+const weatherUrl = process.env.WEATHER_URL;
+const weatherKey = process.env.WEATHER_KEY;
 
 app.use(express.static(path.join(__dirname, '../../client/weather-react/dist/')));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,9 +18,24 @@ app.get('/', (req, res) => {
     reactStream.pipe(res);
 });
 
-app.get('/weather', (req, res) => {
-
+app.get('/weather', async (req, res) => {
+    const url = 'http://api.positionstack.com/v1/forward?' + new URLSearchParams({
+        access_key: process.env.GEOCODE_KEY,
+        query: 'central jerusalem, israel',
+    });
+    console.log('url', url);
+    const data = await fetch(url).then(res => res.json());
+    console.log('data', data);
+    res.send(JSON.stringify(data)).end();
 });
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
+
+// weather url
+/*
+curl 'https://api.open-meteo.com/v1/forecast?latitude=31.77&longitude=35.22&daily=weathercode,temperature_2m_max,temperature_2m_min,uv_index_max,rain_sum,precipitation_hours,windspeed_10m_max&timezone=auto&timeformat=iso8601' | json_pp
+*/
+
+// geocode url
+// http://api.positionstack.com/v1/forward?access_key=6becd3e3ba3f93eaf369c6127bd1f4c8
